4208a51e90edf76d1f0aafbe4a154ac9c2d70760
lqviettt <quocviet30042003@gmail.com>
2024-06-11 15:40:10 +0700

    edit cart

---
+++ gr1/admin/class/order_class.php
@@ -0,0 +1,17 @@
+<?php
+require_once "database.php"
+?>
+
+<?php
+class items{
+    private $db;
+
+    public function __construct(){
+        $this -> db = new Database();
+    }
+    
+    public function show_orders(){
+        
+    }
+}
+?>
